604a5aea0d30ba59d9503f09ee5bd72af845c8f2
Khotso Mokhethi <Mokhetkc@hotmail.com>
2024-12-26 15:10:36 +0200

    Syntax
    Learning the how to add and return numbers.

--- Learn to Program with C++ 17/Learn to Program with C++ 17.cpp
+++ Learn to Program with C++ 17/Learn to Program with C++ 17.cpp
@@ -3,6 +3,23 @@
 
 int main()
 {
-    std::cout << "Hello World! This is khotso Mokhethi\n";
+    std::cout << "Hello World!\n" << "This is khotso Mokhethi\n";
+    int numberOne = 1;
+    int numberTwo = 2;
+   // AddingNumber(numberOne, numberTwo);
+    int x, y;
+    int sum;
+    std::cout << "Type a number: ";
+    std::cin >> x;
+    std::cout << "Type another number: ";
+    std::cin >> y;
+    sum = x + y;
+    std::cout << "Sum is: " << sum;
+}
+
+int AddingNumber(int numberOne, int numberTwo) {
+
+    int returnedNumber = numberOne + numberTwo;
+    return returnedNumber;
 }
 
