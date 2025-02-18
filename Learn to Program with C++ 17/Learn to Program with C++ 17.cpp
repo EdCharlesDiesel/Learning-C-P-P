a61e93c6bd3fd4187c3339f2b58ca2f3b673ff5f
Khotso Mokhethi <Mokhetkc@hotmail.com>
2025-02-18 13:10:12 +0200

    Initial commit
    Learnig C++

--- Learn to Program with C++ 17/Learn to Program with C++ 17.cpp
+++ Learn to Program with C++ 17/Learn to Program with C++ 17.cpp
@@ -1,25 +1,44 @@
-
 #include <iostream>
+#include <iomanip>
 
-int main()
-{
-    std::cout << "Hello World!\n" << "This is khotso Mokhethi\n";
-    int numberOne = 1;
-    int numberTwo = 2;
-   // AddingNumber(numberOne, numberTwo);
-    int x, y;
-    int sum;
-    std::cout << "Type a number: ";
-    std::cin >> x;
-    std::cout << "Type another number: ";
-    std::cin >> y;
-    sum = x + y;
-    std::cout << "Sum is: " << sum;
+void SolveLinearEquations(double matrix[3][4], int n) {
+    // Gaussian Elimination
+    for (int i = 0; i < n; i++) {
+        // Make the diagonal element 1
+        double diagElement = matrix[i][i];
+        for (int j = 0; j <= n; j++) {
+            matrix[i][j] /= diagElement;
+        }
+
+        // Make the other elements in the column 0
+        for (int k = 0; k < n; k++) {
+            if (k == i) continue;
+
+            double factor = matrix[k][i];
+            for (int j = 0; j <= n; j++) {
+                matrix[k][j] -= factor * matrix[i][j];
+            }
+        }
+    }
+
+    // Output the solutions
+    std::cout << "Solutions:" << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cout << "x" << (i + 1) << " = " << std::fixed << std::setprecision(2) << matrix[i][n] << std::endl;
+    }
 }
 
-int AddingNumber(int numberOne, int numberTwo) {
+int main() {
+    // Coefficients matrix
+    double matrix[3][4] = {
+        { 1,  1,  2,  9 },   // Row 1: x1 + x2 + 2x3 = 9
+        { 2,  4, -3,  1 },   // Row 2: 2x1 + 4x2 - 3x3 = 1
+        { 3,  6, -5,  0 }    // Row 3: 3x1 + 6x2 - 5x3 = 0
+    };
 
-    int returnedNumber = numberOne + numberTwo;
-    return returnedNumber;
+    int n = sizeof(matrix) / sizeof(matrix[0]); // Number of equations
+
+    SolveLinearEquations(matrix, n);
+
+    return 0;
 }
-
